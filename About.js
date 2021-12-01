2c8bd63c7d7d50ddf0835045fb43945c51aa1cbb
Thaileaf <lethaison2019@gmail.com>
2021-12-01 00:59:46 -0500

    First commit

---
+++ About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {
+  View,
+  Text
+} from 'react-native';
+
+export default function About() {
+  return(
+    <View style={{ backgroundColor: "blue"}}>
+
+    </View>
+  )
+}
--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+} from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import WelcomeScreen from "./WelcomeScreen";
+import About from "./About";
+
+const Stack = createStackNavigator();
+
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name={"Test"}
+          component={WelcomeScreen}
+          options={{ headerShown:false }}
+        />
+        <Stack.Screen
+          name={"About"}
+          component={About}
+          options={{ headerShown:false }}
+        />
+        <Stack.Screen
+          name={"Services"}
+          component={WelcomeScreen}
+          options={{ headerShown:false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
