689589cfb861310b55082747e2aa41747a280971
hannahyuwen <hz2788@nyu.edu>
2022-11-13 14:05:00 -0500

    feat: welcome button welcomescreen

--- Screens/WelcomeScreen.js
+++ Screens/WelcomeScreen.js
@@ -6,43 +6,7 @@ import WelcomeButton from "./components/welcomeComponent/WelcomeButton";
 function WelcomeScreen({navigation}) {
   return (
     <ScreenTemplate>
-      <Button
-        title="About"
-        color="#601c8c"
-        onPress={
-          () => navigation.navigate('About')
-          // Alert.alert('My Title', 'My message', [
-          //   {text: 'Yes', onPress: () => console.log('Yes')},
-          //   {text: 'No', onPress: () => console.log('No')},
-          // ])
-        }
-      />
-      <Button
-        title="Services"
-        color="#601c8c"
-        onPress={() =>
-          Alert.alert('My Title', 'My Message', [
-            {text: 'Got it', onPress: () => navigation.navigate('Services')},
-          ])
-        }
-      />
-      <Button
-        title="Search Library Resources"
-        color="#601c8c"
-        onPress={() => navigation.navigate('Resources')}
-      />
-      <Button
-        title="Reservable Study Spaces"
-        color="#601c8c"
-        onPress={() => navigation.navigate('RoomReservationStack')}
-      />
-      <Button
-        title="ARTour"
-        color="#601c8c"
-        onPress={() => navigation.navigate('ARVision')}
-      />
-      <Text onPress={() => console.log('Hi')}>Debug Code!</Text>
-    </ScreenTemplate>
+    <WelcomeButton/>
   );
 }
 
